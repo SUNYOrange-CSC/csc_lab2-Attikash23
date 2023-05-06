68d783561f8a8e6333125bc3a0a6e343c256d214
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 22:36:34 -0400

    Add files via upload

---
+++ LM 02 code.cpp
@@ -0,0 +1,45 @@
+
+
+// Jack Rein
+#include <iostream>
+using namespace std;
+
+
+const string FAVORITESODA = "Dr. Dolittle"; 
+const char BESTRATING = 'A'; 
+
+int main(){
+  // LM 2.1
+/*cout<< "Jack Rein"<< endl;
+cout << "32 Rockridge rd" << endl;
+  cout<<endl;
+  cout<<endl;
+cout<< "21113"<< endl;
+cout<< "800-888-1212"<< endl;*/
+  
+  //LM 2.3
+ /* int length = 8;
+  int width = 3;
+  int Area = length * width;
+  int perimeter = (length * 2) + (width * 2);
+  cout<< perimeter<< endl;
+  cout<< Area << endl;*/
+
+  //LM 2.4
+ /* char rating = 'B';
+string favoriteSnack = "crackers";
+int numberOfPeople = 250;
+int topChoiceTotal = 148;
+
+cout << "The preferred soda is " << FAVORITESODA<< endl;
+cout << "The preferred snack is " <<favoriteSnack << endl;
+cout << "Out of " << numberOfPeople << " people "
+<<topChoiceTotal << " chose these items!" << endl;
+cout << "Each of these products were given a rating of " <<BESTRATING ;
+cout << " from our expert tasters" << endl;
+cout << "The other products were rated no higher than a " << rating
+<< endl;*/
+  
+  return 0;
+
+  }
